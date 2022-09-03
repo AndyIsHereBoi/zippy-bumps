0db06233cecdf03e198e25c6fe8fe3fbfb239314
AndyIsHereBoi <69745145+AndyIsHereBoi@users.noreply.github.com>
2022-09-03 14:12:30 -0500

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,11 +1,29 @@
-from nextcord.ext import commands
-from nextcord import slash_command, Interaction
+
 import nextcord
+from nextcord.ext import commands, tasks
+from nextcord.ext.commands.flags import F
+from nextcord import Client, Interaction, SlashOption, ChannelType, Message, slash_command
+from nextcord.abc import GuildChannel
+import nextcord
+import os
 import time
 import datetime
 import json
-config_file = "config.json"
-with open("config.json") as config_file:
+
+replace = os.path.basename(__file__)
+thing = __file__
+directory = thing.replace(f"/cogs/{replace}", '')
+directory = directory + "/"
+
+config_file = f"{directory}config.json"
+import json
+with open(config_file) as config_file:
+    data = json.load(config_file)
+
+
+config_file = f"{directory}config.json"
+import json
+with open(config_file) as config_file:
     data = json.load(config_file)
 
 owners = data['owners']
@@ -20,81 +38,69 @@ footerText = data['footerText']
 ad_count_send = data['ad_count_send']
 
 
-class Status(nextcord.ui.View):
-    @nextcord.ui.button(label='Online', style=nextcord.ButtonStyle.green)
-    async def one(button: nextcord.ui.Button, interaction: nextcord.Interaction):
-        if interaction.user.id in owners:
-            await bot.change_presence(status=nextcord.Status.online)
-            await interaction.response.send_message("Edited status to Online.", ephemeral=True)
-        else:
-            await interaction.response.send_message("You cannot use this.", ephemeral=True)
-                
-    @nextcord.ui.button(label='Idle', style=nextcord.ButtonStyle.blurple)
-    async def two(button: nextcord.ui.Button, interaction: nextcord.Interaction):
-        if interaction.user.id in owners:
-            await bot.change_presence(status=nextcord.Status.idle)
-            await interaction.response.send_message("Edited status to Idle.", ephemeral=True)
-        else:
-            await interaction.response.send_message("You cannot use this.", ephemeral=True)
-
-    @nextcord.ui.button(label='DND', style=nextcord.ButtonStyle.red)
-    async def three(button: nextcord.ui.Button, interaction: nextcord.Interaction):
-        if interaction.user.id in owners:
-            await bot.change_presence(status=nextcord.Status.dnd)
-            await interaction.response.send_message("Edited status to DND.", ephemeral=True)
-        else:
-            await interaction.response.send_message("You cannot use this.", ephemeral=True)
-
 class Owner(commands.Cog):
     """Receives owner commands"""
-    
-    @slash_command(name="ping",description="Bot latency",guild_ids=[794684213697052712], force_global=True)
-    async def ping_command(interaction: Interaction):
-        await interaction.response.defer()
-        start_time = time.time()
-        msg = await interaction.followup.send("Testing Ping...")
-        end_time = time.time()
-        await msg.edit(content=f"Pong!\nBot latency: {round(bot.latency * 1000)}ms\nAPI latency: {round((end_time - start_time) * 1000)}ms")
-    
-    @commands.command()
-    async def ping(ctx):
-        start_time = time.time()
-        message = await ctx.send("Testing Ping...")
-        end_time = time.time()
-        await message.edit(content=f"Pong!\nAPI there: {round(bot.latency * 1000)}ms\nAPI there and back: {round((end_time - start_time) * 1000)}ms")
-    
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
 
+    class Status(nextcord.ui.View):
+        @nextcord.ui.button(label='Online', style=nextcord.ButtonStyle.green)
+        async def one(button: nextcord.ui.Button, interaction: nextcord.Interaction):
+            if interaction.user.id in owners:
+                await bot.change_presence(status=nextcord.Status.online)
+                await interaction.response.send_message("Edited status to Online.", ephemeral=True)
+            else:
+                await interaction.response.send_message("You cannot use this.", ephemeral=True)
+                    
+        @nextcord.ui.button(label='Idle', style=nextcord.ButtonStyle.blurple)
+        async def two(button: nextcord.ui.Button, interaction: nextcord.Interaction):
+            if interaction.user.id in owners:
+                await bot.change_presence(status=nextcord.Status.idle)
+                await interaction.response.send_message("Edited status to Idle.", ephemeral=True)
+            else:
+                await interaction.response.send_message("You cannot use this.", ephemeral=True)
+
+        @nextcord.ui.button(label='DND', style=nextcord.ButtonStyle.red)
+        async def three(button: nextcord.ui.Button, interaction: nextcord.Interaction):
+            if interaction.user.id in owners:
+                await bot.change_presence(status=nextcord.Status.dnd)
+                await interaction.response.send_message("Edited status to DND.", ephemeral=True)
+            else:
+                await interaction.response.send_message("You cannot use this.", ephemeral=True)
+
+    
     @slash_command(name="status",description="Change my status",guild_ids=[794684213697052712], force_global=True)
-    async def status_command(interaction: Interaction, choice: str = SlashOption(name="type", description="Online, Idle, DND, Offline", choices={"Online": "Online", "Idle": "Idle","DND": "DND"})):
+    async def status_command(self, interaction: Interaction, choice: str = SlashOption(name="type", description="Online, Idle, DND, Offline", choices={"Online": "Online", "Idle": "Idle","DND": "DND"})):
         await interaction.response.defer()
         if interaction.user.id in owners:
             if choice == "Online":
                 await interaction.followup.send("Set status to Online")
-                await bot.change_presence(status=nextcord.Status.online)
+                await self.bot.change_presence(status=nextcord.Status.online)
             if choice == "Idle":
                 await interaction.followup.send("Set status to Idle")
-                await bot.change_presence(status=nextcord.Status.idle)
+                await self.bot.change_presence(status=nextcord.Status.idle)
             if choice == "DND":
                 await interaction.followup.send("Set status to DND")
-                await bot.change_presence(status=nextcord.Status.dnd)
+                await self.bot.change_presence(status=nextcord.Status.dnd)
         else:
             await interaction.followup.send("You must be a owner to perform this action.")
 
     @commands.command()
-    async def status(ctx):
+    async def status(self, ctx):
         if ctx.author.id in owners:
             await ctx.send('Select bot status:', view=Status())
 
 
     @commands.command(brief="[Owner Only] Set the bots watching activity")
-    async def activity(ctx, activity):
+    async def activity(self, ctx, activity):
         if ctx.author.id in owners:
-            await bot.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.watching, name=activity))
+            await self.bot.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.watching, name=activity))
                     
                     
     
     @commands.command(brief="[Owner Only] Delete messages")
-    async def purge(ctx, ammount):
+    async def purge(self, ctx, ammount):
         if ctx.author.id in owners:
             ammount = int(ammount)
             if ammount > 100:
@@ -106,5 +112,27 @@ class Owner(commands.Cog):
                 await ctx.channel.purge(limit=ammount)
 
 
+    
+    @slash_command(name="activity",description="Change my playing status!",guild_ids=[859610420615446538], force_global=True)
+    async def activity_command(self, interaction: Interaction, status: str):
+        if interaction.user.id in owners:
+            await interaction.response.defer()
+            await self.bot.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.playing, name=f"{status}"))
+            embed = nextcord.Embed(title="Changed status", description=f'to {status}', color=0xEE8700)
+            embed.set_footer(text=footerText)
+            await interaction.followup.send(embed=embed)
+
+    
+    @commands.command(brief="[Owner Only] send messages from bot")
+    async def say(self, ctx, msg):
+        if ctx.author.id in owners:
+            await ctx.send(msg)
+
+    @slash_command(name="say",description="Make me say something!",guild_ids=[859610420615446538], force_global=True)
+    async def say_command(self, interaction: Interaction, str: str):
+        if interaction.user.id in owners:
+            await interaction.response.defer()
+            await interaction.followup.send(f"{str}")
+
 def setup(bot: commands.Bot):
-    bot.add_cog(Owner())
+    bot.add_cog(Owner(bot))
