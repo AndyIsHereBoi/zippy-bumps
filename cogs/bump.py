ea161020f37c10535fa534fdc68308e686916ed9
AndyIsHereBoi <69745145+AndyIsHereBoi@users.noreply.github.com>
2022-09-03 14:17:41 -0500

    Update bump.py

--- cogs/bump.py
+++ cogs/bump.py
@@ -1,11 +1,21 @@
-from nextcord.ext import commands
-from nextcord import slash_command, Interaction
 import nextcord
+from nextcord.ext import commands, tasks
+from nextcord.ext.commands.flags import F
+from nextcord import Client, Interaction, SlashOption, ChannelType, Message, slash_command
+from nextcord.abc import GuildChannel
 import time
+import os
 import datetime
 import json
-config_file = "config.json"
-with open("config.json") as config_file:
+
+replace = os.path.basename(__file__)
+thing = __file__
+directory = thing.replace(f"/cogs/{replace}", '')
+directory = directory + "/"
+
+config_file = f"{directory}config.json"
+import json
+with open(config_file) as config_file:
     data = json.load(config_file)
 
 owners = data['owners']
@@ -22,14 +32,17 @@ ad_count_send = data['ad_count_send']
 
 class Bump(commands.Cog):
     """Receives bump commands"""
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
     
     @slash_command(name="bump",description="Bump your servers!",guild_ids=[859610420615446538], force_global=True)
-    async def bump_command(interaction: Interaction):
+    async def bump_command(self, interaction: Interaction):
         await interaction.response.defer()
         
-        await interaction.followup.send("I cannot find the `#zb-bump` channel. You must have it to post ads, and recieve ads.")
+        await interaction.followup.send("exmaple")
 
 
 
 def setup(bot: commands.Bot):
-    bot.add_cog(Bump())
+    bot.add_cog(Bump(bot))
